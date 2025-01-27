aa6b553851b6b1edb6b659ca0c6f1834bb704095
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2025-01-27 12:15:57 +0100

    add sync-time.bat

---
+++ sync-time.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Aktiviert den Windows-Zeitdienst und stellt den Starttyp auf "Automatisch".
+sc config w32time start= auto
+net start w32time
+
+REM Konfiguriert den Zeitdienst mit dem NTP-Server "time.windows.com".
+w32tm /config /manualpeerlist:"time.windows.com,0x1" /syncfromflags:manual /update
+
+REM Synchronisiert die Uhrzeit sofort.
+w32tm /resync
+
+REM Abschlussmeldung
+echo Der Windows-Zeitdienst wurde erfolgreich konfiguriert und synchronisiert.
+pause
